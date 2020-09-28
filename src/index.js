b739c2b04018e4585dddde449a1e1845d1de67bd
Alexander Bozhko <obozhko82@gmail.com>
2020-09-28 17:11:32 +0300

    fix bugs, make some functional: default accordion, clear calculator with click anather forms, webpack, babel

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import sendForm                 from './modules/sendForm.js';
+import validateForm             from './modules/validateForm.js';
+import listener                 from './modules/listener.js';
+
+
+sendForm();
+validateForm();
+listener();
+
+
+
+
